2d5151e2b9261b876bbcaf82ca0e99f5e96fb101
Joel Kuntz <joel.kuntz.92@gmail.com>
2016-12-25 22:03:51 -0400

    deploy script v1

---
+++ deploy.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# Inspired by the work of https://github.com/eldarlabs/ghpages-deploy-script
+
+# Store the most recent commit hash
+commit=$(git rev-parse --short HEAD)
+
+# abort the script if there is a non-zero error
+set -e
+
+# Log some stuff
+echo "Starting deploy of $commit"
+pwd
+git status
+
+# Setup git config
+git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
+git config --global user.name "$GH_NAME" > /dev/null 2>&1
+
+# Our test already built it, now we switch to master,
+# replace with newest build, and clean it up
+git checkout master
+find . -not -name 'CNAME' \
+       -not -path './_site*' \
+       -not -path './.git*' \
+       -delete
+mv _site/* .
+rm -rf circle.yml \
+       Rakefile* \
+       deploy.sh \
+       Gemfile*
+
+# Commit it
+git add -A
+git commit -m "Deploy $commit"
+git push origin master
+
+echo "Finished Deployment!"
